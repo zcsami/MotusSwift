eb0ca65e8528f82cd5ccec2ca36e31e41dc85bf4
zcsami <zcsami@gmail.com>
2025-02-03 11:50:23 +0100

    Initial Commit

---
+++ Motus/MotusApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotusApp.swift
+//  Motus
+//
+//  Created by Sami on 03/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct MotusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
